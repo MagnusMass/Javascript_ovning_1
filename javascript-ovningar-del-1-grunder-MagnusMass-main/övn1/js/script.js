2e8006cab0cfd17344d673efb6d69bd5be1b4266
MagnusMass <77322545+MagnusMass@users.noreply.github.com>
2021-01-20 18:00:37 +0200

    Add files via upload

---
+++ javascript-ovningar-del-1-grunder-MagnusMass-main/övn1/js/script.js
@@ -0,0 +1,24 @@
+console.log("Detta är uppgift 1");
+
+let Hej;
+Hej = 'Hej på dig';
+
+let heltal;
+heltal = '3';
+
+let decimaltal;
+decimaltal = '0.3';
+
+let devide;
+devide = 'heltal/decimaltal';
+
+let myclass;
+myclass = 'PU20H';
+
+let myschool;
+myschool = 'Prakticum';
+
+let x;
+x = (3*4.765) / 2 
+
+Math.ceil(x);
